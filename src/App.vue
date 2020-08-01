3e6a86c06ae28a2bba086cc62bbd8c0a3dda8705
knike-d <knike.develop@gmail.com>
2020-08-01 14:38:18 +0900

    topの作成とscssファイル分割

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="app">
+<div id="app">
+  <div id="top-wrap">
+    <div id="top-title">人口推移グラフ</div>
+    <div id="top-sub-title">都道府県別の人口推移グラフを生成します。</div>
   </div>
+</div>
 </template>
 
 <script>
@@ -16,7 +20,32 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
+  #top-wrap{
+    height: clampVal($top-wrap-h);
+    background-color: $top-bg-color;
+    #top-title{
+      max-width: $max-w-multiplier * 100px;
+      font-size: clampVal($top-title-fs, 6);
+      padding-top: clampVal($top-title-top-p);
+      padding-left: clampVal($top-title-left-p);
+      margin: auto;
+      color: black;
+      font-weight: bold;
+      @include fade-in-bottom();
+    }
+    #top-sub-title{
+      max-width: $max-w-multiplier * 100px;
+      font-size: clampVal($top-sub-title-fs, 6);
+      padding-top: clampVal($top-sub-title-top-p);
+      padding-left: clampVal($top-title-left-p);
+      margin: auto;
+      color: black;
+      font-weight: bold;
+      @include fade-in-bottom(.6s);
+    }
+  }
 }
+
 </style>
