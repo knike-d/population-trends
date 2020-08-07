d928f7aa0a843964c3f39a0e01a73241e01b74ce
knike-d <knike.develop@gmail.com>
2020-08-07 14:05:15 +0900

    グラフ表示機能を実装

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,83 @@
       <div id="top-title">人口推移グラフ</div>
       <div id="top-sub-title">都道府県別の人口推移グラフを生成します。</div>
     </div>
-    <PrefSlctGroup />
+    <PrefSlctGroup @emit-pref-list="receivePref" />
+    <PrefChart :data="chartData" :options="options" :css-classes="chartStyle" />
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import PrefSlctGroup from "@/components/PrefSlctGroup.vue";
+import PrefChart from "@/components/PrefChart.vue";
 
 export default {
   name: "app",
   components: {
     PrefSlctGroup,
+    PrefChart,
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [],
+        datasets: [],
+      },
+      options: {
+        animation: false,
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "年",
+              },
+            },
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "人",
+              },
+            },
+          ],
+        },
+      },
+      chartStyle: "pref-chart",
+    };
+  },
+  methods: {
+    async receivePref(list) {
+      const prefUrl = "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear";
+      const labels = [];
+      const datasets = [];
+      for (const i in list.prefCodeList) {
+        const prefData = {};
+        await axios
+          .get(prefUrl, {
+            headers: { "X-API-KEY": "" },
+            params: {
+              prefCode: list.prefCodeList[i],
+              cityCode: "-",
+            },
+          })
+          .then((res) => {
+            const resData = res.data.result.data[0].data;
+            Object.assign(
+              labels,
+              resData.map((obj) => obj.year)
+            );
+            prefData.label = list.prefNameList[i];
+            prefData.data = resData.map((obj) => obj.value);
+            prefData.borderColor = `hsl(${(360 / list.prefCodeList.length) * i}, 100%, 65%)`;
+            prefData.fill = false;
+            datasets.push(prefData);
+          });
+      }
+      this.chartData.labels = labels;
+      this.chartData.datasets = datasets;
+    },
   },
 };
 </script>
@@ -49,6 +115,13 @@ export default {
       font-size: clamping($top-sub-title-fs, 6);
       @include fade-in-bottom(0.6s);
     }
+  }
+  .pref-chart {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: clamping($chart-w);
+    margin: clamping($pref-chart-m) auto;
   }
 }
 </style>
---
+++ src/components/PrefChart.vue
@@ -0,0 +1,26 @@
+<script>
+import { Line, mixins } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  mixins: [mixins.reactiveData],
+  props: {
+    data: {
+      type: Object,
+    },
+    options: {
+      type: Object,
+    },
+  },
+  mounted() {
+    this.chartData = this.data;
+    this.renderChart(this.chartData, this.options);
+  },
+  watch: {
+    "data.datasets"() {
+      this.chartData = this.data;
+      this.renderChart(this.chartData, this.options);
+    },
+  },
+};
+</script>
